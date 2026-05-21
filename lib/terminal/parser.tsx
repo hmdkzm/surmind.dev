aac3a0f404d2430f1f6d55912fc73a5e62c40f82
hmdkzm <surrmind@gmail.com>
2026-05-21 23:17:55 +0200

    feat: add some test program to parser

--- lib/terminal/parser.tsx
+++ lib/terminal/parser.tsx
@@ -3,20 +3,24 @@ import { commands } from './commands'
 
 export const parser = (prompt: string, terminal: TerminalSlice['terminal']) => {
   const [prog, command, ...args] = prompt.split(' ')
-  if (prog !== 'tr') return false
-  if (command === 'set') {
-    const [property, value] = args
-    if (commands.set[property]) {
-      terminal.setState({
-        theme: { ...terminal.theme, [commands.set[property]]: value },
-      })
-    } else {
-      return ['type tr help', 'Invalid Params']
+  if (prog === 'tr') {
+    if (command === 'set') {
+      const [property, value] = args
+      if (commands.set[property]) {
+        terminal.setState({
+          theme: { ...terminal.theme, [commands.set[property]]: value },
+        })
+      } else {
+        return ['type tr help', 'Invalid Params']
+      }
+      return
     }
-    return
   }
-  if (command === 'help') {
+  if (prog === 'help') {
     return "there's no help for now"
   }
-  return 'Not Found'
+  if (prog === 'clock') {
+    return [new Date().toDateString() + ' ' + new Date().toLocaleTimeString()]
+  }
+  return ['Not Found']
 }
